52ac449ac441c76cb99e94e2103a2ce7043125f4
stayade <sarthak.tayade@prorigosoftware.com>
2013-10-03 02:40:40 +0530

    Adding a link to the git hub project for the abs-and-crypto project

--- html/index/project.php
+++ html/index/project.php
@@ -3,6 +3,10 @@
 </div>
 
 <div class="project-container">
-    <a href="project/androidcryptolib.php" class="project-link"><h3 class="project-title"">Android Encryption/Decryption Library</h3></a>
+    <div>
+        <a href="project/androidcryptolib.php" class="project-link"><h3 class="project-title"">AbsAndCrypto</h3></a>
+        <a href="https://github.com/abslog/abs-and-crypto" class="project-git-link"><img src="github-logo.gif"></a>
+        <span class="clear"></span>
+    </div>
     <p class="project-desc">  A library project to help android applications encrypt or decrypt content in the background using an android service.</p>
 </div>
