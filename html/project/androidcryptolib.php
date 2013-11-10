7664a3666c3f2a1149f1fe49b907c6b6fbaffdd9
abslog <abstrucelogic@gmail.com>
2013-11-10 14:08:16 +0530

    Adding description for AbsAndCrypto

--- html/project/androidcryptolib.php
+++ html/project/androidcryptolib.php
@@ -16,10 +16,45 @@
                     <li><a class="tab-title" href="../#about">About</a></li>
                 </ul>
             </div>
-        </div>
-        
-        <div>
-            <h1 style="text-align: center"> Project info will be uploaded soon. Sorry for the inconvinience</h1>
+            <div class="row">
+                <div class="span2 project-left-menu">
+                    <div data-spy="affix" style="margin-top: 10px">
+                        <ul class="nav nav-tabs nav-stacked">
+                            <li><a href="#overview">Overview</a></li>
+                            <li><a href="#description">Description</a></li>
+                            <li><a href="#download">Download</a></li>
+                            <li><a href="#howtouse">How to use?</a></li>
+                            <li><a href="#sample">Sample</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="span10">
+                    <div>
+                        <h1 class="project-title-desc">AbsAndCrypto<h1>
+                    </div>
+                    <div id="overview" class="project-info-block">
+                        <h1 class="project-info-header">Overview</h1>
+                        <p class="project-info-body">A library project which enables applications to encrypt/decrypt files in the background using a service or an async task.</p>
+                    </div>
+                    <div id="description" class="project-info-block">
+                        <h1 class="project-info-header">Description</h1>
+                        <p class="project-info-body">It is aimed at making encrytion and decryption of files as simple as possible for client applications. This process can be performed on a backgroud thread (AsyncTask) or an android service as per the configuration provided. The library also takes the liberty of displaying a notification with the current progress of the crypto operation.</p>
+                    </div>
+                    <div id="download" class="project-info-block">
+                        <h1 class="project-info-header">Download</h1>
+                        <p class="project-info-body">There is no binary distribution for this library because Android Library projects are not ment to be used as binaries. Instead, please clone the git repository of the project or download a the source code as a zip file from github.</p>
+                    </div>
+                    <div id="howtouse" class="project-info-block">
+                        <h1 class="project-info-header">How to use?</h1>
+                        <p class="project-info-body">To use this library, you need to include it in your java classpath when compiling / building your application.</p>
+                    </div>
+                    <div id="sample" class="project-info-block">
+                        <h1 class="project-info-header">Sample</h1>
+                        <!-- <p class="project-info-body">An opensource project using this library is available at - (git link).</p> -->
+                        <p class="project-info-body"> Sample to be uploaded shortly.</p>
+                    </div>
+                </div>
+            </div>
         </div>
         
         <script src="../ui/lib/jquery/jquery-2.0.3.js"></script>
